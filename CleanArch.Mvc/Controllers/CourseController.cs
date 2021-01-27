df34a5a3f4233cdfca022779d4cd2029434d82c0
Jorge Andrés Torres Morales <55928840+jorgeandres2125@users.noreply.github.com>
2021-01-27 07:36:36 -0500

    Clen Code in .NET Framework

---
+++ CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService) 
+        {
+            _courseService = courseService;
+        }
+
+        private string GetHost(Microsoft.AspNetCore.Http.HttpContext context)
+        {
+            var host = $"{context.Request.Scheme}://{context.Request.Host}";
+            return host;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["CurrentHost"] = GetHost(HttpContext);
+            CourseViewModel model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
